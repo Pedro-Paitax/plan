5306259144d35ee50e5605c0a3ed9f72df4275be
Pedro Paitax <pedrohemriquepaitax@gmail.com>
2023-03-29 19:30:29 -0300

    feat: :art: Conversor - Main Files

---
+++ conversor/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './components/page/Form';
+import Header from './components/header/Header';
+
+function App() {
+  return (
+    <><header>
+    <Header />
+  </header>
+  <Form /></>
+    
+  );
+}
+
+export default App;
